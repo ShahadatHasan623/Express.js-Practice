14b9a1b5ba2e494609850c0b374cc09f3f4a8e3a
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-05-15 11:35:48 +0600

    added get all users data fetching

--- src/server.ts
+++ src/server.ts
@@ -58,6 +58,29 @@ app.post("/api/users", async (req: Request, res: Response) => {
   }
 });
 
+app.get('/api/users',async(req:Request,res:Response)=>{
+    try {
+        const result = await pool.query(`
+            SELECT * FROM users
+            `)
+        if(result.rows.length === 0){
+           return res.status(404).json({
+            message:"Not Found",
+            data:{}
+           })
+        }
+        return res.status(200).json({
+            message:"all users fetch to data succesfully",
+            data:result.rows
+        })
+    } catch (error:any) {
+        res.status(500).json({
+            message:error.message,
+            error:error
+        })
+    }
+})
+
 app.listen(port, () => {
   console.log(`server app listening on port ${port}`);
 });
