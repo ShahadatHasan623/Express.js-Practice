54fd6baa7fa6f2a3ed63dd5f7f0869924f580477
ShahadatHasan623 <shahadathasan743599@gmail.com>
2026-05-15 10:37:20 +0600

    added database connected

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express, { type Application, type Request, type Response } from "express"
+import {Pool} from "pg"
 const app:Application = express()
 const port = 3000
 
+const pool =new Pool({
+    connectionString:"postgresql://neondb_owner:npg_wf4YXSEhg8zI@ep-shiny-frost-apji3h8w-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+})
+
+
 app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
+  res.status(200).json({
+    message:"server root is running"
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`server app listening on port ${port}`)
 })
